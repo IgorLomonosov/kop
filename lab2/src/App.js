97af1ef1f538d456b494559bfb09b31e100e346e
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2024-11-11 16:34:14 +0200

    Використано компонент з хуком у App.js

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AudioPlayer from './components/AudioPlayer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Custom Audio Player</h1>
+      <AudioPlayer url="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
     </div>
   );
 }

e760df4b60d8da91376b7da565d2aa0914804828
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2024-12-06 17:07:35 +0200

    Added LocalStorage to project to save info when page is refreshed

--- lab5/src/models/contacts.js
+++ lab5/src/models/contacts.js
@@ -4,9 +4,30 @@ export const addContact = createEvent();
 export const updateContact = createEvent();
 export const deleteContact = createEvent();
 
-export const $contacts = createStore([])
-  .on(addContact, (state, contact) => [...state, { id: Date.now(), ...contact }])
-  .on(updateContact, (state, updatedContact) =>
-    state.map((contact) => (contact.id === updatedContact.id ? updatedContact : contact))
-  )
-  .on(deleteContact, (state, id) => state.filter((contact) => contact.id !== id));
+const loadContactsFromLocalStorage = () => {
+  const storedContacts = localStorage.getItem('contacts');
+  return storedContacts ? JSON.parse(storedContacts) : [];
+};
+
+const saveContactsToLocalStorage = (contacts) => {
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+};
+
+export const $contacts = createStore(loadContactsFromLocalStorage())
+  .on(addContact, (state, contact) => {
+    const updatedState = [...state, { id: Date.now(), ...contact }];
+    saveContactsToLocalStorage(updatedState);
+    return updatedState;
+  })
+  .on(updateContact, (state, updatedContact) => {
+    const updatedState = state.map((contact) =>
+      contact.id === updatedContact.id ? updatedContact : contact
+    );
+    saveContactsToLocalStorage(updatedState);
+    return updatedState;
+  })
+  .on(deleteContact, (state, id) => {
+    const updatedState = state.filter((contact) => contact.id !== id);
+    saveContactsToLocalStorage(updatedState);
+    return updatedState;
+  });
